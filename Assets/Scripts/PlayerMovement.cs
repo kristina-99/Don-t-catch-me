47794cc21ef5aa0039ecb479c19a158378229ee5
kristina-99 <kristina_atan@abv.bg>
2026-05-08 13:36:47 +0300

    * added horizontal movement to the Player * added flip method * updated the animation state scheme

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D myRigidbody2D;
     private Vector2 moveInput;
+    private float playerSpeed = 10f;
+
 
     void Start()
     {
@@ -13,11 +15,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        Run();
+        Flip();
     }
 
     void OnMove(InputValue moveInputValue)
     {
         moveInput = moveInputValue.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, myRigidbody2D.linearVelocity.y);
+        myRigidbody2D.linearVelocity = playerVelocity;
+    }
+
+    void Flip()
+    {
+        bool isMoving = Mathf.Abs(myRigidbody2D.linearVelocity.x) > Mathf.Epsilon;
+        if(isMoving)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.linearVelocity.x),1f);
+        }
     }
 }
